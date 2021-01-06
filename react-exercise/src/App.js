8668d17df0dbf7f6a61c28abfd96720597251824
cfor0 <cfor0@outlook.com>
2021-01-06 09:29:39 -0500

    completed veryEasy challenge

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Very Easy Challenge
+      <p>Name: Carlo Ortega</p>
+      <p>Number: 7049571252</p>
+      <p>DOB: 01/17/1997</p> */}
     </div>
   );
 }
