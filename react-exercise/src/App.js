247ac749d288340f6ec38d9e8082075ba3a4cca6
cfor0 <cfor0@outlook.com>
2021-01-06 09:41:49 -0500

    completed easy challenge

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Very Easy Challenge
+class App extends Component() {
+
+  // Easy Challenge
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     person: {}
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        {/* Very Easy Challenge
       <p>Name: Carlo Ortega</p>
       <p>Number: 7049571252</p>
       <p>DOB: 01/17/1997</p> */}
-    </div>
-  );
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
